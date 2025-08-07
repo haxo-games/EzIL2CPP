cca2ef91e29638894b4890ad1cb705231b7f80ea
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2025-08-07 10:09:25 -0700

    feat: Added little structures

--- README.md
+++ README.md
@@ -12,10 +12,27 @@ TODO
 
 There isn't really any documentation regarding the IL2CPP API functions so I decided it could be useful to document them as best as we can here.
 
+### `il2cpp_domain_get()`
+
+Returns the value of the global pointer to the domain object. If said pointer is 0 then it initializes the domain.
+
+### `il2cpp_domain_get_assemblies(void* p_domain, uint64_t* p_assembly_count)`
+
+Takes in both the target domain (does nothing with it) and a pointer to an 8 bytes location to write the amount of assemblies to. To calculate the size
+it subtracts the global point to the end of the assemblies list from the one to the start of it. Finally it returns the pointer to the start of the assemblies.
+
+### `il2cpp_domain_assembly_open(void* p_domain, const char* assembly_name)`
+
+Takes in both the target domain (does nothing with it) and the target assembly's name. Under the hood it simply goes through the list of assemblies and compares each of
+their names with the one provided until there is a match, in which case it would return the pointer to the matched assembly.
+
+### `il2cpp_assembly_get_image(void* p_assembly)`
+
+Takes in the pointer to an assembly object and returns the pointer the image object it contains.
+
 ## Contributing
 
 This project is free and open source and will remain so forever. You are welcome to contribute. Simply make a pull request for whatever it is you would like to add, but
-
 there are a few things you need to keep in mind:
 
 1. C++17 only for now.
--- include/EzIL2CPP.h
+++ include/EzIL2CPP.h
@@ -16,154 +16,167 @@
 
 #include <windows.h>
 
-class EzIL2CPP
+namespace EzIL2CPP
 {
-public:
-	void* (*il2cpp_domain_get)();
-	void* (*il2cpp_domain_get_assemblies)(void* p_domain, uint64_t* assembly_count);
-	void* (*il2cpp_domain_assembly_open)(void* p_domain, const char* name);
-	void* (*il2cpp_assembly_get_image)(void* p_assembly);
-	void* (*il2cpp_thread_attach)(void* p_domain);
-	void* (*il2cpp_thread_detach)(void* p_domain);
-	void* (*il2cpp_runtime_invoke)(void* p_method, void* p_object, void** params, void** exceptions);
-	void* (*il2cpp_class_from_name)(void* p_image, const char* namespaze, const char* name);
-	void* (*il2cpp_class_get_type)(void* p_class);
-	void* (*il2cpp_type_get_object)(void* p_type);
-	void* (*il2cpp_class_get_method_from_name)(void* p_class, const char* name, int args_count);
-	void* (*il2cpp_class_get_field_from_name)(void* p_class, const char* iter);
-	void* (*il2cpp_field_get_value)(void* p_obj, void* p_field, void* p_value);
-
-	EzIL2CPP(HMODULE h_game_assembly)
+	struct Image
 	{
-		initialize(h_game_assembly);
-	}
+		const char* name;
+	};
 
-	bool initialize(HMODULE h_game_assembly)
+	struct Assembly
 	{
-		if (is_initialized)
+		Image* p_image;
+	};
+
+	class Resolver
+	{
+	public:
+		void* (*il2cpp_domain_get)();
+		Assembly** (*il2cpp_domain_get_assemblies)(void* p_domain, uint64_t* p_assembly_count);
+		Assembly* (*il2cpp_domain_assembly_open)(void* p_domain, const char* name);
+		Image* (*il2cpp_assembly_get_image)(void* p_assembly);
+		void* (*il2cpp_thread_attach)(void* p_domain);
+		void* (*il2cpp_thread_detach)(void* p_domain);
+		void* (*il2cpp_runtime_invoke)(void* p_method, void* p_object, void** params, void** exceptions);
+		void* (*il2cpp_class_from_name)(void* p_image, const char* namespaze, const char* name);
+		void* (*il2cpp_class_get_type)(void* p_class);
+		void* (*il2cpp_type_get_object)(void* p_type);
+		void* (*il2cpp_class_get_method_from_name)(void* p_class, const char* name, int args_count);
+		void* (*il2cpp_class_get_field_from_name)(void* p_class, const char* iter);
+		void* (*il2cpp_field_get_value)(void* p_obj, void* p_field, void* p_value);
+
+		Resolver(HMODULE h_game_assembly)
+		{
+			initialize(h_game_assembly);
+		}
+
+		bool initialize(HMODULE h_game_assembly)
+		{
+			if (is_initialized)
+				return true;
+
+			if (h_game_assembly == 0)
+			{
+				setError("initialize(): h_game_assembly was NULL");
+				return false;
+			}
+
+			this->h_game_assembly = h_game_assembly;
+
+			if (!resolveImports())
+				return false;
+
+			is_initialized = true;
 			return true;
-
-		if (h_game_assembly == 0)
-		{
-			setError("initialize(): h_game_assembly was NULL");
-			return false;
 		}
 
-		this->h_game_assembly = h_game_assembly;
-
-		if (!resolveImports())
-			return false;
-
-		is_initialized = true;
-		return true;
-	}
-
-	const std::string& getErrorMessage() const
-	{
-		return error_message;
-	}
-
-	bool getHasError() const
-	{
-		return has_error;
-	}
-
-	bool getIsInitialized() const
-	{
-		return is_initialized;
-	}
-
-	void setError(const char* fmt, ...)
-	{
-		/* Clear error if empty message */
-		if (strlen(fmt) == 0)
+		const std::string& getErrorMessage() const
 		{
-			has_error = false;
-			error_message.clear();
-			return;
+			return error_message;
 		}
 
-		va_list args;
-		va_start(args, fmt);
-
-		char* buf{ new char[0x1000] };
-		vsprintf_s(buf, 0x1000, fmt, args);
-		error_message = buf;
-
-		delete[] buf;
-		va_end(args);
-		has_error = true;
-	}
-
-private:
-	bool is_initialized{};
-	std::string error_message;
-	bool has_error{};
-	HMODULE h_game_assembly;
-
-	void* resolveSingleImport(IMAGE_EXPORT_DIRECTORY* p_export_directory, const char* target_export)
-	{
-		static DWORD* address_table{ reinterpret_cast<DWORD*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_export_directory->AddressOfFunctions) };
-		static DWORD* name_table{ reinterpret_cast<DWORD*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_export_directory->AddressOfNames) };
-		static WORD* ordinal_table{ reinterpret_cast<WORD*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_export_directory->AddressOfNameOrdinals) };
-
-		for (size_t i{}; i < p_export_directory->NumberOfNames; i++)
+		bool getHasError() const
 		{
-			const char* export_name{ reinterpret_cast<const char*>(reinterpret_cast<uintptr_t>(h_game_assembly) + name_table[i]) };
-
-			if (strcmp(export_name, target_export) == 0)
-				return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(h_game_assembly) + address_table[ordinal_table[i]]);
+			return has_error;
 		}
 
-		return nullptr;
-	}
-
-	template<typename T> 
-	inline void assignImport(T& p_function, IMAGE_EXPORT_DIRECTORY* p_export_directory, const char* target_export)
-	{
-		void* result{ resolveSingleImport(p_export_directory, target_export) };
-		p_function = reinterpret_cast<decltype(p_function)>(result);
-	}
-
-	bool resolveImports()
-	{
-		IMAGE_DOS_HEADER* p_dos_header{ reinterpret_cast<IMAGE_DOS_HEADER*>(h_game_assembly) };
-
-		if (p_dos_header->e_magic != IMAGE_DOS_SIGNATURE)
+		bool getIsInitialized() const
 		{
-			setError("resolveImports(): DOS header magic is invalid");
-			return false;
+			return is_initialized;
 		}
 
-		IMAGE_NT_HEADERS* p_nt_headers{ reinterpret_cast<IMAGE_NT_HEADERS*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_dos_header->e_lfanew) };
-
-		if (p_nt_headers->Signature != IMAGE_NT_SIGNATURE)
+		void setError(const char* fmt, ...)
 		{
-			setError("resolveImports(): NT signature is invalid");
-			return false;
+			/* Clear error if empty message */
+			if (strlen(fmt) == 0)
+			{
+				has_error = false;
+				error_message.clear();
+				return;
+			}
+
+			va_list args;
+			va_start(args, fmt);
+
+			char* buf{ new char[0x1000] };
+			vsprintf_s(buf, 0x1000, fmt, args);
+			error_message = buf;
+
+			delete[] buf;
+			va_end(args);
+			has_error = true;
 		}
 
-		if (p_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size == 0)
+	private:
+		bool is_initialized{};
+		std::string error_message;
+		bool has_error{};
+		HMODULE h_game_assembly;
+
+		void* resolveSingleImport(IMAGE_EXPORT_DIRECTORY* p_export_directory, const char* target_export)
 		{
-			setError("resolveImports(): The export directory of the game assembly is marked with no entries");
-			return false;
+			static DWORD* address_table{ reinterpret_cast<DWORD*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_export_directory->AddressOfFunctions) };
+			static DWORD* name_table{ reinterpret_cast<DWORD*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_export_directory->AddressOfNames) };
+			static WORD* ordinal_table{ reinterpret_cast<WORD*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_export_directory->AddressOfNameOrdinals) };
+
+			for (size_t i{}; i < p_export_directory->NumberOfNames; i++)
+			{
+				const char* export_name{ reinterpret_cast<const char*>(reinterpret_cast<uintptr_t>(h_game_assembly) + name_table[i]) };
+
+				if (strcmp(export_name, target_export) == 0)
+					return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(h_game_assembly) + address_table[ordinal_table[i]]);
+			}
+
+			return nullptr;
 		}
 
-		IMAGE_EXPORT_DIRECTORY* p_export_directory{ reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress) };
-		assignImport(il2cpp_domain_get, p_export_directory, "il2cpp_domain_get");
-		assignImport(il2cpp_domain_get_assemblies, p_export_directory, "il2cpp_domain_get_assemblies");
-		assignImport(il2cpp_domain_assembly_open, p_export_directory, "il2cpp_domain_assembly_open");
-		assignImport(il2cpp_assembly_get_image, p_export_directory, "il2cpp_assembly_get_image");
-		assignImport(il2cpp_thread_attach, p_export_directory, "il2cpp_thread_attach");
-		assignImport(il2cpp_thread_detach, p_export_directory, "il2cpp_thread_detach");
-		assignImport(il2cpp_runtime_invoke, p_export_directory, "il2cpp_runtime_invoke");
-		assignImport(il2cpp_class_from_name, p_export_directory, "il2cpp_class_from_name");
-		assignImport(il2cpp_class_get_type, p_export_directory, "il2cpp_class_get_type");
-		assignImport(il2cpp_type_get_object, p_export_directory, "il2cpp_type_get_object");
-		assignImport(il2cpp_class_get_method_from_name, p_export_directory, "il2cpp_class_get_method_from_name");
-		assignImport(il2cpp_class_get_field_from_name, p_export_directory, "il2cpp_class_get_field_from_name");
-		assignImport(il2cpp_field_get_value, p_export_directory, "il2cpp_field_get_value");
+		template<typename T>
+		inline void assignImport(T& p_function, IMAGE_EXPORT_DIRECTORY* p_export_directory, const char* target_export)
+		{
+			void* result{ resolveSingleImport(p_export_directory, target_export) };
+			p_function = reinterpret_cast<decltype(p_function)>(result);
+		}
 
-		return true;
-	}
-};
+		bool resolveImports()
+		{
+			IMAGE_DOS_HEADER* p_dos_header{ reinterpret_cast<IMAGE_DOS_HEADER*>(h_game_assembly) };
+
+			if (p_dos_header->e_magic != IMAGE_DOS_SIGNATURE)
+			{
+				setError("resolveImports(): DOS header magic is invalid");
+				return false;
+			}
+
+			IMAGE_NT_HEADERS* p_nt_headers{ reinterpret_cast<IMAGE_NT_HEADERS*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_dos_header->e_lfanew) };
+
+			if (p_nt_headers->Signature != IMAGE_NT_SIGNATURE)
+			{
+				setError("resolveImports(): NT signature is invalid");
+				return false;
+			}
+
+			if (p_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size == 0)
+			{
+				setError("resolveImports(): The export directory of the game assembly is marked with no entries");
+				return false;
+			}
+
+			IMAGE_EXPORT_DIRECTORY* p_export_directory{ reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(reinterpret_cast<uintptr_t>(h_game_assembly) + p_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress) };
+			assignImport(il2cpp_domain_get, p_export_directory, "il2cpp_domain_get");
+			assignImport(il2cpp_domain_get_assemblies, p_export_directory, "il2cpp_domain_get_assemblies");
+			assignImport(il2cpp_domain_assembly_open, p_export_directory, "il2cpp_domain_assembly_open");
+			assignImport(il2cpp_assembly_get_image, p_export_directory, "il2cpp_assembly_get_image");
+			assignImport(il2cpp_thread_attach, p_export_directory, "il2cpp_thread_attach");
+			assignImport(il2cpp_thread_detach, p_export_directory, "il2cpp_thread_detach");
+			assignImport(il2cpp_runtime_invoke, p_export_directory, "il2cpp_runtime_invoke");
+			assignImport(il2cpp_class_from_name, p_export_directory, "il2cpp_class_from_name");
+			assignImport(il2cpp_class_get_type, p_export_directory, "il2cpp_class_get_type");
+			assignImport(il2cpp_type_get_object, p_export_directory, "il2cpp_type_get_object");
+			assignImport(il2cpp_class_get_method_from_name, p_export_directory, "il2cpp_class_get_method_from_name");
+			assignImport(il2cpp_class_get_field_from_name, p_export_directory, "il2cpp_class_get_field_from_name");
+			assignImport(il2cpp_field_get_value, p_export_directory, "il2cpp_field_get_value");
+
+			return true;
+		}
+	};
+}
